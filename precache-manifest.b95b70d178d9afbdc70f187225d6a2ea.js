3c5d2f7bd5ba19d1bdd8f4bdff540da51f65a8b9
savantm1 <savantm1@mail.ru>
2020-06-11 00:03:34 +0300

    Updates

---
+++ precache-manifest.b95b70d178d9afbdc70f187225d6a2ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1646fadf6483dd7266a1345b74e8aa33",
+    "url": "/First-react-project/index.html"
+  },
+  {
+    "revision": "04adc2c1ac5d70b7c17a",
+    "url": "/First-react-project/static/css/main.3ad42cdc.chunk.css"
+  },
+  {
+    "revision": "fc778e500b7f48874037",
+    "url": "/First-react-project/static/js/2.643e92b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/First-react-project/static/js/2.643e92b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04adc2c1ac5d70b7c17a",
+    "url": "/First-react-project/static/js/main.60533602.chunk.js"
+  },
+  {
+    "revision": "66e78fed7fe7e6b17861",
+    "url": "/First-react-project/static/js/runtime-main.2fbe4e2a.js"
+  }
+]);
