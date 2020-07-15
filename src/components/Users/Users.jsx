ba829710f4401be440701b0735aa3387f4d0a439
savantm1 <savantm1@mail.ru>
2020-07-15 22:07:20 +0300

    51-52 взаимодействие  с API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
-import styles from './Users.module.css';
+import * as axios from "axios";
+import styles from "./Users.module.css"
 import User from './User/User';
+import avatar_default from "../../assets/images/avatar_default.png";
+
 
 let Users = (props) => {
 
- let userElement = props.users.map((element)=> {
-  return (<User
-   avatar={element.avatar}
+  let setUsers = () => {
+    if (props.users.length === 0) {
+
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(Response => {
+        props.setUsers( Response.data.items)
+  })
+  
+    }
+  }
+
+  
+
+
+  let userElement = props.users.map((element) => {
+    return (<User 
+     avatar={element.photos.small == null ? avatar_default : element.photos.small}
    status={element.status}
    name={element.name}
-   county={element.country}
-   city={element.city}
+   county={'element.country'}
+   city={'element.city'}
    btnText={element.followed ? "Follow" : "Unfollow"} onClick={element.followed ? () => { props.unfollow(element.id) } : () => { props.follow(element.id) }} />)
  })
  return (
-  
-  < div className={styles.users}>
-   <h3 className={styles.title}> Users</h3>
+   < div className={styles.users}>
+     <h3 className={styles.title}> Users</h3>
+     <button onClick={setUsers}>btn</button>
    {userElement}
   </div >
  )
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -4,15 +4,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = "SET-USERS";
 
 let initialState = {
- users: [
-    {
-      id: 1, avatar: 'https://avatars.mds.yandex.net/get-pdb/1352825/a5f8fbd7-515e-49f8-81d6-be55a2daac92/s1200',
-      followed: false, name: 'Dima', country: 'Belarus', city: 'Minsk', status: 'Whats up? Follow me!'
-    },
-  { id: 2, avatar: 'https://avatars.mds.yandex.net/get-pdb/1352825/a5f8fbd7-515e-49f8-81d6-be55a2daac92/s1200',followed: true, name: 'Rita', country: 'Russia', city: 'Moscow', status: 'Kukusik!' },
-  { id: 3, avatar: 'https://avatars.mds.yandex.net/get-pdb/1352825/a5f8fbd7-515e-49f8-81d6-be55a2daac92/s1200',followed: false, name: 'Anna', country: 'Ukraine', city: 'Kiev', status: 'I like bikes!' },
-  { id: 4, avatar: 'https://avatars.mds.yandex.net/get-pdb/1352825/a5f8fbd7-515e-49f8-81d6-be55a2daac92/s1200',followed: false, name: 'Alex', country: 'USA', city: 'New York', status: 'YEAAH!' },
- ]
+ users: []
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -46,7 +38,7 @@ const usersReducer = (state = initialState, action) => {
   }
    
   case SET_USERS: {
-   return { ...state, users: action.users}
+   return { ...state, users:[ ...state.users, ...action.users]}
    }
   default:
    return state;
