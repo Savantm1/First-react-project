a177badb38108efc2a6c1cd7328dbc922c7b923f
savantm1 <savantm1@mail.ru>
2020-08-02 21:59:21 +0300

    56

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,49 @@
-// import React from 'react';
-// import * as axios from "axios";
-// import styles from "./Users.module.css"
-// import User from './User/User';
-// import avatar_default from "../../assets/images/avatar_default.png";
+import React from "react";
+import styles from "./Users.module.css";
+import User from "./User/User";
+import avatar_default from "../../assets/images/avatar_default.png";
 
 
-// let Users = (props) => {
 
-//   let setUsers = () => {
-//     if (props.users.length === 0) {
 
-//       axios.get('https://social-network.samuraijs.com/api/1.0/users').then(Response => {
-//         props.setUsers( Response.data.items)
-//   })
+let Users = (props) => {
+
+ let userElement = props.users.map((element) => {
+  return (<User
+   avatar={element.photos.small == null ? avatar_default : element.photos.small}
+ status={element.status}
+ name={element.name}
+ county={'element.country'}
+ city={'element.city'}
+ btnText={element.followed ? "Follow" : "Unfollow"} onClick={element.followed ? () => { props.unfollow(element.id) } : () => { props.follow(element.id) }} />)
+})
   
-//     }
-//   }
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++){
+    pages.push(i);
+  }
 
-  
-
-
-//   let userElement = props.users.map((element) => {
-//     return (<User 
-//      avatar={element.photos.small == null ? avatar_default : element.photos.small}
-//    status={element.status}
-//    name={element.name}
-//    county={'element.country'}
-//    city={'element.city'}
-//    btnText={element.followed ? "Follow" : "Unfollow"} onClick={element.followed ? () => { props.unfollow(element.id) } : () => { props.follow(element.id) }} />)
-//  })
-//  return (
-//    < div className={styles.users}>
-//      <h3 className={styles.title}> Users</h3>
-//      <button onClick={setUsers}>btn</button>
-//    {userElement}
-//   </div >
-//  )
+ return (
  
-// }
+  < div className={styles.users}>
+  <h3 className={styles.title}> Users</h3>
+  <ul className={styles.paginator}>
+  
+    {pages.map(p => { 
+      return <li className={props.currentPage === p ? styles.item__active : styles.item} onClick={(e) =>{props.onPageChanged(p)} }>{p}</li>
+        }
+  
+       )}
+   
+   </ul>
+   
+   {userElement}
+   
+  </div >
+  )
+  
+}
 
-// export default Users;
+
+export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,45 @@
 import { followAC, setUsersAC, unfollowAC, setCurrentPageAC, setUsersTotalCountAC } from "../../redux/usersReducer";
 import { connect } from "react-redux";
-import Users from "./UsersC";
+import * as axios from "axios";
+import Users from './Users';
+import React from 'react';
+
+
+class UsersAPIComponent extends React.Component {
+
+  componentDidMount() {
+
+  axios.get(`https://social-network.samuraijs.com/api/1.0/users/?count=${this.props.pageSize}&page=${this.props.currentPage}`).then(Response => {
+    this.props.setUsers(Response.data.items);
+    this.props.setTotalUsersCount(Response.data.totalCount);
+  })
+  };
+
+  
+  
+
+  onPageChanged = (pageNumber)=> {
+    
+      this.props.setCurrentPage(pageNumber)
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users/?count=${this.props.pageSize}&page=${pageNumber}`).then(Response => {
+        this.props.setUsers( Response.data.items)
+      })
+    }
+  
+
+  render() {
+
+    return (<Users
+      onPageChanged={this.onPageChanged}
+      pageSize={this.props.pageSize}
+      currentPage={this.props.currentPage}
+      totalUsersCount={this.props.totalUsersCount}
+      users={this.props.users}
+      follow={this.props.follow}
+      unfollow={this.props.unfollow}
+    />)
+  }
+}
 
 
 let mapStateToProps = (state) => {
@@ -32,5 +71,5 @@ let mapDispatchToProps =(dispatch)=> {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users)
+export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent)
 
